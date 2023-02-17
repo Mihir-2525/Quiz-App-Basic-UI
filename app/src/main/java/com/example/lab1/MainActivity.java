3a196ef3195127a5219befc9421fbd328e53fb66
Mihir-2525 <113364270+Mihir-2525@users.noreply.github.com>
2023-02-17 23:54:35 +0530

    add java code in button

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.lab1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.upgrade);
+        b1.setOnClickListener(view -> {
+            Intent in =new Intent(MainActivity.this,mcq.class);
+            startActivity(in);
+        });
     }
 }
